7a6664ea4c4e69c0af6b75ba766ed8cc7e065406
Talor Anderson <ta.anders17@gmail.com>
2019-12-04 14:23:05 -0800

    add copy to clipboard and overflow

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react'
 import './App.css'
+
+const defaultTitle  ='ascii art maker :)'
+const copiedTitle = 'copied to clipboard!'
+
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');  // Create a <textarea> element
+  el.value = str;                                 // Set its value to the string that you want copied
+  el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+  el.style.position = 'absolute';                 
+  el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+  document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+  const selected =            
+    document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+      ? document.getSelection().getRangeAt(0)     // Store selection if found
+      : false;                                    // Mark as false to know no selection existed before
+  el.select();                                    // Select the <textarea> content
+  document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+  document.body.removeChild(el);                  // Remove the <textarea> element
+  if (selected) {                                 // If a selection existed before copying
+    document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+    document.getSelection().addRange(selected);   // Restore the original selection
+  }
+};
+
 class App extends Component {
   state = {
     figure: '',
-    text: ''
+    text: '',
+    title: defaultTitle
   }
-componentDidMount() {
+  componentDidMount() {
     this.fetchFig()
   }
-fetchFig = async () => {
+  fetchFig = async () => {
     const response = await fetch(`/api/fig`)
     const initial = await response.json()
     const figure = initial.fig
     this.setState({ figure })
   }
-customFig = async evt => {
+  customFig = async evt => {
     evt.preventDefault()
     const text = this.state.text
     const response = await fetch(`/api/fig/${text}`)
@@ -22,15 +47,26 @@ customFig = async evt => {
     const figure = custom.fig
     this.setState({ figure, text: '' })
   }
-handleChange = evt => {
+  handleChange = evt => {
     this.setState({ [evt.target.name]: evt.target.value })
     console.log(this.state.text)
   }
-render() {
+
+  onFigClick = () => {
+    copyToClipboard(this.state.figure)
+    this.setState({ title: copiedTitle });
+    setTimeout(()=> this.setState({title: defaultTitle}), 1000)
+  }
+  render() {
     return (
       <div className="App">
-        <h3>ascii art maker :)</h3>
-        <code>{this.state.figure}</code>
+  <h3>{this.state.title}</h3>
+        <div className="wrap">
+
+          <code onClick={this.onFigClick}>
+            {this.state.figure}
+          </code>
+        </div>
         <form onSubmit={this.customFig}>
           <label>Custom Text:</label>
           <input
@@ -42,6 +78,9 @@ render() {
           />
           <button type="submit">make it!</button>
         </form>
+        <div className="footer">
+          created by <a href="//taloranderson.com">Talor Anderson</a>
+        </div>
       </div>
     )
   }
